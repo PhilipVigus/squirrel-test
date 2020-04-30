43657f0ed06f270a100f9e6dc638684d961891af
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-30 12:16:13 +0100

    Add toNotEqual matcher

--- squirrelTest.js
+++ squirrelTest.js
@@ -5,10 +5,17 @@ function it(label, test) {
 
 function expect(conditionUnderTest) {
   toEqual: function toEqual(expectedResult) {
-    conditionUnderTest === expectedResult ? console.log("Passed") : console.log("Test failed");
+    conditionUnderTest === expectedResult ? console.log("Passed") : console.log("Failed");
   }
 
-  return { toEqual: toEqual };
+  toNotEqual: function toNotEqual(expectedResult) {
+    conditionUnderTest !== expectedResult ? console.log("Passed") : console.log("Failed");
+  }
+
+  return { 
+    toEqual: toEqual,
+    toNotEqual: toNotEqual
+  };
 }
 
 it("confirms that 3 is equal to 2 + 1", () => {
@@ -17,4 +24,12 @@ it("confirms that 3 is equal to 2 + 1", () => {
 
 it("confirms that 4 is equal to 2 + 1", () => {
   expect(4).toEqual(2 + 1);
+});
+
+it("confirms that 4 is not equal to 2 + 1", () => {
+  expect(4).toNotEqual(2 + 1);
+});
+
+it("confirms that 4 is not equal to 2 + 1", () => {
+  expect(3).toNotEqual(2 + 1);
 });
